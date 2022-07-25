ee6e607c5b2912e72ba2979f000ea86ea2af6641
Zeng, Wen-Feng <jalew188@gmail.com>
2022-07-25 08:58:26 +0200

    df based reader

---
+++ alpharaw/pysciexwifffilereader.py
@@ -0,0 +1,135 @@
+import os
+import sys
+import numpy as np
+import time
+
+from alpharaw.pythermorawfilereader import DotNetArrayToNPArray
+from alpharaw.utils.centroiding import centroid_data
+
+# require pythonnet, pip install pythonnet on Windows
+import clr
+clr.AddReference('System')
+import System
+from System.Threading import Thread
+from System.Globalization import CultureInfo
+
+de_fr = CultureInfo('fr-FR')
+other = CultureInfo('en-US')
+
+Thread.CurrentThread.CurrentCulture = other
+Thread.CurrentThread.CurrentUICulture = other
+
+dir = os.path.dirname(os.path.abspath(__file__))
+clr.AddReference(os.path.join(dir, "ext/sciex/Clearcore2.Data.AnalystDataProvider.dll"))
+clr.AddReference(os.path.join(dir, "ext/sciex/ClearCore2.Data.dll"))
+clr.AddReference(os.path.join(dir, "ext/sciex/WiffOps4Python.dll"))
+import Clearcore2
+import WiffOps4Python
+from WiffOps4Python import WiffOps as DotNetWiffOps
+from Clearcore2.Data.AnalystDataProvider import (
+    AnalystWiffDataProvider,
+    AnalystDataProviderFactory
+)
+
+class WillFileReader:
+    def __init__(self, filename:str):
+
+        self._wiffDataProvider = AnalystWiffDataProvider()
+        self._wiff_file = AnalystDataProviderFactory.CreateBatch(
+            filename, self._wiffDataProvider
+        )
+        self.sample_names = self._wiff_file.GetSampleNames()
+
+    def close(self): self._wiffDataProvider.Close()
+
+    def load_sample(self, sample_id:int, centroid:bool=True):
+        if sample_id < 0 or sample_id >= len(self.sample_names):
+            raise ValueError("Incorrect sample number.")
+        self.wiffSample = self._wiff_file.GetSample(sample_id)
+        self.msSample = self.wiffSample.MassSpectrometerSample
+
+        _peak_indices = []
+        cycle_id_list = []
+        experiment_id_list = []
+        peak_mz_array_list = []
+        peak_intensity_array_list = []
+        rt_list = []
+        ms_level_list = []
+        precursor_mz_list = []
+        precursor_charge_list = []
+        ce_list = []
+        precursor_lower_mz_list = []
+        precursor_upper_mz_list = []
+
+        exp_list = [
+            self.msSample.GetMSExperiment(i) 
+            for i in range(self.msSample.ExperimentCount)
+        ]
+
+        for j in range(exp_list[0].Details.NumberOfScans):
+            for i in range(self.msSample.ExperimentCount):
+                exp = exp_list[i]
+                massSpectrum = exp.GetMassSpectrum(j)
+                if massSpectrum.NumDataPoints <= 0: continue
+                details = exp.Details
+                massSpectrumInfo = exp.GetMassSpectrumInfo(j)
+                peak_mz_array_list.append(
+                    DotNetArrayToNPArray(massSpectrum.GetActualXValues())
+                )
+                peak_intensity_array_list.append(
+                    DotNetArrayToNPArray(massSpectrum.GetActualYValues())
+                )
+                _peak_indices.append(len(peak_mz_array_list[-1]))
+                cycle_id_list.append(j)
+                experiment_id_list.append(i)
+                rt_list.append(exp.GetRTFromExperimentCycle(j))
+                # rt_list.append(exp.GetRTFromExperimentScanIndex(j))
+                # ScanMode = massSpectrumInfo.CentroidMode ? WiffFile.ScanMode.Centroid : WiffFile.ScanMode.Profile,
+                # DataTitle = massSpectrumInfo.GetDescription(true),
+                # PeaksCount = massSpectrum.NumDataPoints,
+                # BasePeakMz = (max_idx == -1) ? 0.0 : actualXValues[max_idx],
+                # BasePeakIntensity = (float)((max_idx == -1) ? 0.0 : actualYValues[max_idx]),
+                # Polarity = (details.Polarity == MSExperimentInfo.PolarityEnum.Positive) ? WiffFile.Polarity.Positive : WiffFile.Polarity.Negative,
+                # low_mz = details.StartMass
+                # high_mz = details.EndMass
+                ms_level_list.append( massSpectrumInfo.MSLevel)
+                # ScanType = (details.IDAType == MSExperimentInfo.IDAExperimentType.Survey) ? WiffFile.ScanType.MS1 : WiffFile.ScanType.MS2,
+                # FragmentationType = "NULL"
+
+                center_mz = -1
+                isolation_window = 0
+                if ms_level_list[-1] > 1:
+                    if details.IsSwath and details.MassRangeInfo.Length > 0:
+                        center_mz = DotNetWiffOps.get_center_mz(details)
+                        isolation_window = DotNetWiffOps.get_isolation_window(details)
+                    precursor_mz_list.append(massSpectrumInfo.ParentMZ)
+                    precursor_charge_list.append(massSpectrumInfo.ParentChargeState)
+                    ce_list.append(float(massSpectrumInfo.CollisionEnergy))
+                    precursor_lower_mz_list.append(center_mz-isolation_window/2)
+                    precursor_upper_mz_list.append(center_mz+isolation_window/2)
+                else:
+                    precursor_mz_list.append(-1.0)
+                    precursor_charge_list.append(-1.0)
+                    ce_list.append(-1.0)
+                    precursor_lower_mz_list.append(-1.0)
+                    precursor_upper_mz_list.append(-1.0)
+
+                    # scanInfo.ScanMode = WiffFile.ScanMode.Centroid
+        
+        peak_indices = np.empty(len(rt_list) + 1, np.int64)
+        peak_indices[0] = 0
+        peak_indices[1:] = np.cumsum(_peak_indices)
+
+        return {
+            "peak_indices": peak_indices, 
+            "peak_mz": np.concatenate(peak_mz_array_list),
+            "peak_intensity": np.concatenate(peak_intensity_array_list),
+            "experiment_id": np.array(experiment_id_list, dtype=np.int64), 
+            "cycle_id": np.array(cycle_id_list, dtype=np.int64), 
+            "rt": np.array(rt_list, dtype=np.float64), 
+            "ms_level": np.array(ms_level_list, dtype=np.int8), 
+            "precursor_mz": np.array(precursor_mz_list, dtype=np.float64), 
+            "precursor_charge": np.array(precursor_charge_list, dtype=np.int8), 
+            'precursor_mz_lower': np.array(precursor_lower_mz_list),
+            'precursor_mz_upper': np.array(precursor_upper_mz_list),
+        }
---
+++ alpharaw/sciex.py
@@ -0,0 +1,54 @@
+import numpy as np
+import pandas as pd
+import os
+import alpharaw.pysciexwifffilereader as pysciexwifffilereader
+from alpharaw.ms_data_base import MSData_Base
+    
+class SciexWiffData(MSData_Base):
+    def __init__(self, centroided:bool=True):
+        super().__init__(centroided)
+
+    def _import(self,
+        _wiff_file_path:str
+    )->dict:
+        wiff_reader = pysciexwifffilereader.WillFileReader(
+            _wiff_file_path
+        )
+        data_dict = wiff_reader.load_sample(0)
+        wiff_reader.close()
+        return data_dict
+    
+    def _set_dataframes(self, raw_data:dict):
+        self.create_spectrum_df(len(raw_data['rt']))
+        self.set_peaks_by_cat_array(
+            raw_data['peak_mz'],
+            raw_data['peak_intensity'],
+            raw_data['peak_indices'][:-1],
+            raw_data['peak_indices'][1:],
+        )
+        self.add_column_in_spec_df(
+            'rt', raw_data['rt']
+        )
+        self.add_column_in_spec_df(
+            'ms_level', raw_data['ms_level'],
+            dtype=np.int8
+        )
+        self.set_precursor_mz(
+            raw_data['precursor_mz']
+        )
+        self.add_column_in_spec_df(
+            'charge', raw_data['precursor_charge'],
+            dtype=np.int8
+        )
+        self.set_precursor_mz_windows(
+            raw_data['precursor_mz_lower'],
+            raw_data['precursor_mz_upper'],
+        )
+        self.add_column_in_spec_df(
+            'experiment_id', raw_data['experiment_id'],
+            dtype=np.int32
+        )
+        self.add_column_in_spec_df(
+            'cycle_id', raw_data['cycle_id'],
+            dtype=np.int32
+        )
