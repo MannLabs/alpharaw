5b769fea8b10924a08b0d601aea8e3f89db93725
zhouxiexuan <zhouxiexuan@qq.com>
2023-12-05 10:15:46 +0100

    based plotting lib for ms data

---
+++ alpharaw/viz/df_utils.py
@@ -0,0 +1,251 @@
+import typing
+
+import pandas as pd
+import numpy as np
+
+from alpharaw.match.match_utils import (
+    match_closest_peaks, match_highest_peaks
+)
+
+from alphabase.peptide.precursor import (
+    calc_precursor_mz,
+    calc_precursor_isotope_intensity,
+)
+
+from alphabase.peptide.fragment import (
+    create_fragment_mz_dataframe,
+    flatten_fragments
+)
+
+from alphabase.constants.modification import MOD_MASS
+
+def make_plot_df(
+    sequence: str,
+    mods: str,
+    mod_sites: str,
+    charge: int,
+    rt_sec: float = 0.0,
+    mobility: float = 0.0,
+    include_fragments:bool=True,
+    charged_frag_types: list = ["b_z1","b_z2","y_z1","y_z2"],
+    include_precursor_isotopes:bool=False,
+    max_isotope:int = 6,
+    min_frag_mz:float = 100.0,
+):
+    precursor_df, fragment_mz_df = make_precursor_fragment_df(
+        sequence, mods, mod_sites, charge,
+        include_fragments=include_fragments,
+        charged_frag_types=charged_frag_types,
+        include_precursor_isotopes=include_precursor_isotopes,
+        max_isotope=max_isotope,
+    )
+    return translate_precursor_fragment_df_to_plot_df(
+        precursor_df, fragment_mz_df,
+        rt_sec=rt_sec,
+        mobility=mobility,
+        min_frag_mz=min_frag_mz,
+    )
+
+def make_xic_plot_df(
+    query_masses:np.ndarray,
+    query_ion_names:typing.List[str],
+    query_rt_sec:float, 
+    precursor_mz:float,
+    query_im:float = 0.0,
+    query_intensities:np.ndarray = None,
+)->pd.DataFrame:
+    df = pd.DataFrame(dict(
+        mz=query_masses,
+        ion_name=query_ion_names,
+        rt_sec=query_rt_sec,
+        precursor_mz=precursor_mz,
+    ))
+    if query_im:
+        df["im"] = query_im
+    if query_intensities is not None:
+        df["intensity"] = query_intensities
+    return df
+
+def make_psm_plot_df(
+    spec_masses:np.ndarray,
+    spec_intensities:np.ndarray,
+    query_masses:np.ndarray,
+    ion_names:typing.List[str],
+    query_mass_tols:np.ndarray,
+    query_frag_idxes:np.ndarray,
+    modified_sequence:str = "",
+    query_intensities:np.ndarray = None,
+    match_mode:typing.Literal["closest","highest"]="closest"
+):
+    ion_names = np.array(ion_names, dtype="U")
+    if match_mode == "highest":
+        matched_idxes = match_highest_peaks(
+            spec_masses, spec_intensities,
+            query_mzs=query_masses, 
+            query_mz_tols=query_mass_tols,
+        )
+    else:
+        matched_idxes = match_closest_peaks(
+            spec_masses, spec_intensities,
+            query_mzs=query_masses,
+            query_mz_tols=query_mass_tols
+        )
+    matched_bools = (matched_idxes!=-1)&(query_masses>0)
+    matched_query_masses = query_masses[matched_bools]
+    matched_frag_idxes = query_frag_idxes[matched_bools]
+    matched_ion_names = ion_names[matched_bools]
+    matched_idxes = matched_idxes[matched_bools]
+    matched_spec_masses = spec_masses[matched_idxes]
+    matched_spec_intens = spec_intensities[matched_idxes]
+    matched_mass_errs = matched_spec_masses-matched_query_masses
+    ppm_mass_errs = matched_mass_errs*1e6/matched_query_masses
+
+    spec_df = pd.DataFrame(dict(
+        modified_sequence=modified_sequence,
+        mz=spec_masses,
+        intensity=spec_intensities,
+        fragment_idx=-1,
+        ppm_err=0,
+        mass_err=0,
+        ion_name="-",
+    ))
+    matched_df = pd.DataFrame(dict(
+        modified_sequence=modified_sequence,
+        mz=matched_query_masses,
+        intensity=matched_spec_intens,
+        fragment_idx=matched_frag_idxes,
+        ppm_err=ppm_mass_errs,
+        mass_err=matched_mass_errs,
+        ion_name=matched_ion_names,
+    ))
+
+    if query_intensities is None or len(matched_df) == 0:
+        mirror_df = pd.DataFrame()
+    else:
+        query_intensities *= -(
+            matched_spec_intens.max()
+            /query_intensities[matched_bools].max()
+        )
+        mirror_df = pd.DataFrame(dict(
+            modified_sequence=modified_sequence,
+            mz=query_masses,
+            intensity=query_intensities,
+            fragment_idx=-1,
+            ppm_err=0,
+            mass_err=0,
+            ion_name=ion_names,
+        ))
+
+    return pd.concat([
+        spec_df, matched_df, mirror_df
+    ], ignore_index=True)
+
+def get_modified_sequence(
+    sequence:str, 
+    mods:str, mod_sites:str, 
+    charge:int = 0
+):
+    sequence = '_'+sequence+'_'
+    mod_masses = np.zeros(len(sequence))
+    if mods:
+        for mod, mod_site in zip(
+            mods.split(';'),mod_sites.split(';')
+        ):
+            mod_masses[int(mod_site)] += MOD_MASS[mod]
+        mod_masses = np.rint(mod_masses)
+    mod_seq = sequence
+    for i in range(len(sequence)-1,-1,-1):
+        if mod_masses[i] != 0:
+            mod_seq = (
+                mod_seq[:i+1]
+                +f"[{mod_masses[i]:+.0f}]"
+                +mod_seq[i+1:]
+            )
+    if charge != 0:
+        return mod_seq + f"({charge}+)"
+    else:
+        return mod_seq
+
+def translate_precursor_fragment_df_to_plot_df(
+    precursor_df:pd.DataFrame, 
+    fragment_mz_df:pd.DataFrame, 
+    fragment_intensity_df:pd.DataFrame = None,
+    rt_sec:float = 0.0, 
+    mobility:float = 0.0,
+    min_frag_mz:float = 100.0,
+    min_frag_intensity:float = 0.001,
+):
+    fragment_mz_df = fragment_mz_df.mask(
+        fragment_mz_df<min_frag_mz, 0
+    )
+    if fragment_intensity_df is None:
+        fragment_intensity_df = pd.DataFrame()
+
+    flat_columns = [
+        'type','number','loss_type','charge'
+    ]
+    precursor_df, fragment_df = flatten_fragments(
+        precursor_df, fragment_mz_df, 
+        fragment_intensity_df,
+        min_fragment_intensity=min_frag_intensity,
+        custom_columns=flat_columns+["position"],
+    )
+
+    fragment_df.rename(
+        columns={"position":"fragment_idx"},
+        inplace=True
+    )
+
+    fragment_df["ion_name"] = fragment_df[flat_columns].apply(
+        lambda x: 
+            chr(x[0])+str(x[1])
+            +(f"{-x[2]:+}" if x[2]!=0 else "")
+            +"+"*x[3],
+        axis=1
+    )
+
+    fragment_df["sequence"] = precursor_df.sequence.values[0]
+    fragment_df["modified_sequence"] = get_modified_sequence(
+        sequence=precursor_df.sequence.values[0], 
+        mods=precursor_df.mods.values[0], 
+        mod_sites=precursor_df.mod_sites.values[0], 
+        charge=0 if "charge" not in precursor_df.columns 
+                 else precursor_df.charge.values[0],
+    )
+    if rt_sec:
+        fragment_df["rt_sec"] = rt_sec
+    if mobility:
+        fragment_df["im"] = mobility
+
+    return fragment_df
+    
+def make_precursor_fragment_df(
+    sequence: str,
+    mods: str,
+    mod_sites: str,
+    charge: int,
+    include_fragments:bool=True,
+    charged_frag_types: list = ["b_z1","b_z2","y_z1","y_z2"],
+    include_precursor_isotopes:bool=False,
+    max_isotope:int = 6,
+):
+    precursor_df = pd.DataFrame(dict(
+        sequence=[sequence],
+        mods=[mods],
+        mod_sites=[mod_sites],
+        charge=charge
+    ))
+    calc_precursor_mz(precursor_df)
+    if include_precursor_isotopes:
+        calc_precursor_isotope_intensity(
+            precursor_df, max_isotope=max_isotope
+        )
+    if include_fragments:
+        fragment_mz_df = create_fragment_mz_dataframe(
+            precursor_df, charged_frag_types
+        )
+    else:
+        fragment_mz_df = pd.DataFrame(
+            columns=charged_frag_types
+        )
+    return precursor_df, fragment_mz_df
---
+++ alpharaw/viz/psm_plot.py
@@ -0,0 +1,530 @@
+from plotly.subplots import make_subplots
+from collections import defaultdict
+
+import pandas as pd
+import numpy as np
+
+import typing
+
+import plotly.graph_objects as go
+
+from .plot_utils import plot_scatter
+
+color_map:dict = defaultdict(lambda:"brown")
+color_map.update({
+    '-': 'lightgrey', # umnatched peaks
+    'a': 'darkskyblue',
+    'b': 'blue', 
+    'c': 'skyblue', 
+    'x': 'darkred',
+    'y': 'red',
+    'z': 'deeppink',
+    'M': 'purple', # precursor
+    'Y': 'orange',
+    'B': 'darkgreen',
+})
+
+class PSM_Plot:
+    vertical_spacing = 0.05
+    template = 'plotly_white'
+    plot_height = 600
+    def __init__(self, 
+        peak_plot_rows = 4,
+        mass_err_plot_rows = 1,
+        frag_coverage_plot_rows = 1,
+        frag_coverage_at_top = True
+    ):
+        specs = []
+        if frag_coverage_at_top:
+            specs.append(
+                [{
+                    "rowspan": frag_coverage_plot_rows, 
+                    "colspan": 3
+                }, None, None]
+            )
+            specs.extend(
+                [[None, None, None]]*
+                (frag_coverage_plot_rows-1)
+            )
+        specs.append(
+            [{
+                "rowspan": peak_plot_rows, 
+                "colspan": 3
+            }, None, None]
+        )
+        specs.extend(
+            [[None, None, None]]*
+            (peak_plot_rows-1)
+        )
+        specs.append(
+            [{
+                "rowspan": mass_err_plot_rows, 
+                "colspan": 3
+            }, None, None]
+        )
+        specs.extend(
+            [[None, None, None]]*
+            (mass_err_plot_rows-1)
+        )
+        if not frag_coverage_at_top:
+            specs.append(
+                [{
+                    "rowspan": frag_coverage_plot_rows, 
+                    "colspan": 3
+                }, None, None]
+            )
+            specs.extend(
+                [[None, None, None]]*
+                (frag_coverage_plot_rows-1)
+            )
+
+        if frag_coverage_at_top:
+            (
+                frag_cov_row,
+                peak_row, 
+                mass_err_row,
+            ) = np.cumsum([
+                1, frag_coverage_plot_rows,
+                peak_plot_rows,
+            ])
+        else:
+            (
+                peak_row, 
+                mass_err_row,
+                frag_cov_row, 
+            ) = np.cumsum([
+                1, peak_plot_rows,
+                mass_err_plot_rows
+            ])
+
+        self.specs = specs
+        self.peak_row = peak_row
+        self.mass_err_row = mass_err_row
+        self.frag_cov_row = frag_cov_row
+        self.rows = (
+            peak_plot_rows+mass_err_plot_rows+
+            frag_coverage_plot_rows
+        )
+
+    def plot(self, plot_df, sequence, title, 
+        plot_unmatched_peaks=False
+    ):
+        self._init_plot(title)
+
+        self.peak_plot.plot(
+            plot_df, 
+            plot_unmatched_peaks=plot_unmatched_peaks
+        )
+        self.mass_err_plot.plot(
+            plot_df,
+        )
+        self.frag_cov_plot.plot(
+            plot_df, sequence
+        )
+        return self.fig
+
+    def _init_plot(self, title):
+
+        self.fig = make_subplots(
+            rows=self.rows,
+            cols=3, 
+            shared_xaxes=True,
+            specs=self.specs,
+            vertical_spacing=self.vertical_spacing,
+            column_widths=[0.25, 0.5, 0.25]
+        )
+
+        self.peak_plot = PeakPlot(
+            self.fig, self.peak_row, 
+        )
+        self.mass_err_plot = MassErrPlot(
+            self.fig, self.mass_err_row,
+        )
+        self.frag_cov_plot = FragCoveragePlot(
+            self.fig, self.frag_cov_row
+        )
+
+        self.fig.update_layout(
+            template=self.template,
+            title=dict(
+                text=title,
+                yanchor='bottom'
+            ),
+            hovermode='x',
+            height=self.plot_height,
+        )
+        self.fig.update_xaxes(matches='x')
+        self.fig.update_yaxes(
+            title = 'intensity',
+        )
+
+class MassErrPlot:
+    def __init__(self, fig_subplots, row):
+        """
+        See `MS_Plot_Base` Parameters
+        """
+        self.fig = fig_subplots
+        self.row = row
+        self.col = 1
+        self.hovertemplate = (
+            '%{hovertext}<br>'
+            '<b>m/z:</b> %{x}<br>'
+            '<b>Mass err:</b> %{y}'
+        )
+
+    def plot(self, plot_df:pd.DataFrame):
+        ion_types = plot_df.ion_name.str[0].unique()
+        for ion_type in ion_types:
+            if ion_type == "-": continue
+            self._plot_one_ion(
+                plot_df, ion_type, color_map[ion_type]
+            )
+        self.fig.update_yaxes(
+            title_text='m/z err',
+            row=self.row,col=self.col
+        )
+        return self.fig
+
+    def _plot_one_ion(self,
+        plot_df, ion_type, color
+    ):
+        df = plot_df[
+            plot_df.ion_name.str.startswith(ion_type)
+        ].query("intensity > 0")
+        self.fig.add_trace(
+            plot_scatter(
+                df.mz.values, 
+                df.ppm_err.round(4).values,
+                color=color,
+                marker_size=5,
+                hovertext=df.ion_name.values,
+                hovertemplate=self.hovertemplate,
+                name=f"{ion_type} ion_name",
+            ), 
+            row=self.row,
+            col=self.col
+        )
+
+class PeakPlot:
+    fig:go.Figure
+    row:int=1
+    col:int=1
+    hovertemplate = (
+        '<b>%{hovertext}</b><br>'
+        '<b>m/z:</b> %{x}<br>'
+        '<b>intensity:</b> %{y}'
+    )
+    peak_line_width = 1.5
+
+    def __init__(self, 
+        fig:go.Figure, 
+        row:int,
+        col:int=1
+    ):
+        self.fig = fig
+        self.row = row
+        self.col = col
+        
+    def plot(self,
+        plot_df,
+        plot_unmatched_peaks:bool=True,
+    )->go.Figure:
+        if plot_unmatched_peaks:
+            _df = plot_df[plot_df.ion_name=="-"]
+            self.fig.add_trace(
+                plot_scatter(
+                    _df.mz,
+                    _df.intensity,
+                    color=color_map['-'],
+                    marker_size=1,
+                    hovertext=None,
+                    hovertemplate=None,
+                    name='',
+                ),
+                row=self.row,
+                col=self.col,
+            )
+        
+        for ion_type in np.unique(plot_df.ion_name.str[0].values):
+            self._plot_one_ion_type_scatter(
+                plot_df, ion_type
+            )
+
+        # self._plot_one_ion_type_scatter(
+        #     plot_df, 'b'
+        # )
+        # self._plot_one_ion_type_scatter(
+        #     plot_df, 'y'
+        # )
+        
+        # self._plot_one_ion_type_scatter(
+        #     plot_df, 'c'
+        # )
+        # self._plot_one_ion_type_scatter(
+        #     plot_df, 'z'
+        # )
+
+        self._plot_peak_vlines(
+            plot_df,
+        )
+
+        self._plot_ion_name(
+            plot_df.query('ion_name != "-"')
+        )
+        return self.fig
+
+    def _plot_one_ion_type_scatter(self, 
+        plot_df, ion_type
+    ):
+        df_all = plot_df[
+            plot_df.ion_name.str.startswith(ion_type)
+        ]
+        _df = df_all.query('intensity>0')
+        self.fig.add_trace(
+            plot_scatter(
+                _df.mz,
+                _df.intensity,
+                color=color_map[ion_type], 
+                marker_size=1,
+                hovertext=_df.ion_name,
+                hovertemplate=self.hovertemplate,
+                name='',
+            ),
+            row=self.row,
+            col=self.col,
+        )
+        _df = df_all.query('intensity<0')
+        self.fig.add_trace(
+            plot_scatter(
+                _df.mz,
+                _df.intensity,
+                color=color_map[ion_type], 
+                marker_size=1,
+                hovertext=_df.ion_name,
+                hovertemplate=self.hovertemplate,
+                name='',
+            ),
+            row=self.row,
+            col=self.col,
+        )
+
+    def _plot_ion_name(self, plot_df):
+        df = plot_df.query('intensity>0')
+        max_inten = df.intensity.max()
+        yshift=max_inten*0.02
+        for mz, inten, ion in df[
+            ['mz','intensity','ion_name']
+        ].values:
+            self.fig.add_annotation(
+                x=mz, y=inten+yshift,
+                text=ion,
+                textangle=-90,
+                font_size=10,
+                row=self.row,
+                col=self.col,
+            )
+        
+        pred_df = plot_df.query('intensity<0')
+        pred_df = pred_df[~pred_df.ion_name.isin(set(df.ion_name))]
+        for mz, inten, ion in pred_df[
+            ['mz','intensity','ion_name']
+        ].values:
+            self.fig.add_annotation(
+                x=mz, y=inten-yshift,
+                text=ion,
+                textangle=-90,
+                font_size=10,
+                ay=inten-yshift-max_inten*(0.28+len(ion)/60),
+                ayref=f'y{self.row}',
+                yref=f'y{self.row}',
+                row=self.row,
+                col=self.col,
+            )
+
+    def _plot_peak_vlines(self,
+        plot_df,
+    ):
+        self.fig.update_layout(
+            shapes=[
+                dict(
+                    type='line',
+                    xref=f'x{self.row}',
+                    yref=f'y{self.row}',
+                    x0=plot_df.loc[i, 'mz'],
+                    y0=0,
+                    x1=plot_df.loc[i, 'mz'],
+                    y1=plot_df.loc[i, 'intensity'],
+                    line=dict(
+                        color=color_map[plot_df.loc[i,'ion_name'][0]],
+                        width=self.peak_line_width,
+                    )
+                ) for i in plot_df.index
+            ]
+        )
+        # for i in plot_df.index:
+        #     self.fig.add_shape(type='line',
+        #         x0=plot_df.loc[i, 'mz'],
+        #         x1=plot_df.loc[i, 'mz'],
+        #         y0=0,
+        #         y1=plot_df.loc[i, 'intensity'],
+        #         line_color = color_map[plot_df.loc[i, 'ion_name'][0]],
+        #         line_width = self.peak_line_width,
+        #         row=self.row,
+        #         col=self.col,
+        #     )
+
+class FragCoveragePlot:
+    def __init__(self, fig_subplots, row):
+        """
+        See `MS_Plot_Base` Parameters
+        """
+        self.fig = fig_subplots
+        self.row = row
+        self.col = 1
+        self.font_size_sequence = 14
+        self.font_size_coverage = 8
+
+    def plot(self,
+        plot_df,
+        sequence,
+    ):
+        if len(sequence) > 0:
+            d = (
+                plot_df.mz.max() - 
+                plot_df.mz.min()
+            ) * 2/len(sequence)
+            aa_x_position_name = np.linspace(
+                plot_df.mz.min()+d, 
+                plot_df.mz.max()-d, 
+                len(sequence)+1
+            )
+
+            self._plot_sequence(sequence, aa_x_position_name)
+            for ion_type in np.unique(plot_df.ion_name.str[0]):
+                self._plot_coverage_one_frag_type(
+                    plot_df, sequence, aa_x_position_name, 
+                    ion_type, color_map[ion_type],
+                )
+            # self._plot_coverage_one_frag_type(
+            #     plot_df, sequence, aa_x_position_name, 
+            #     'b', color_map['b'],
+            # )
+            # self._plot_coverage_one_frag_type(
+            #     plot_df, sequence, aa_x_position_name, 
+            #     'y', color_map['y'],
+            # )
+            self.fig.update_yaxes(
+                visible=False,
+                range=(-1.1, 1.1),
+                row=self.row,
+                col=self.col,
+            )
+            self.fig.update_xaxes(
+                visible=False,
+                row=self.row,
+                col=self.col,
+            )
+        return self.fig
+
+    def _plot_sequence(self,
+        sequence, aa_x_position_name,
+    ):
+        for i, aa in enumerate(sequence):
+            self.fig.add_annotation(
+                dict(
+                    text=aa,
+                    x=aa_x_position_name[i],
+                    y=0,
+                    showarrow=False,
+                    font_size=self.font_size_sequence,
+                    yshift=1, align='center'
+                ),
+                row=self.row,
+                col=self.col
+            )
+
+    def _plot_coverage_one_frag_type(self,
+        plot_df, sequence, 
+        aa_x_position_name, 
+        ion_type,
+        color,
+    ):
+        nAA = len(sequence)
+        plot_df = plot_df[
+            plot_df.ion_name.str.startswith(ion_type)
+        ].query("intensity>0")
+
+        covs = np.zeros(max(
+            plot_df.fragment_idx.max()+2 
+            if len(plot_df)>0 else 0,
+            nAA
+        ), dtype=np.int64)
+        if ion_type in 'abc':
+            covs[plot_df.fragment_idx] = 1
+        elif ion_type in 'xyz':
+            covs[plot_df.fragment_idx+1] = 1
+
+        def get_position_name(ion_type, i):
+            if ion_type in 'abc':
+                return dict(
+                    x=[
+                        aa_x_position_name[i], 
+                        aa_x_position_name[i] + (
+                            aa_x_position_name[i+1] - aa_x_position_name[i]
+                        )/2, 
+                        aa_x_position_name[i] + (
+                            aa_x_position_name[i+1] - aa_x_position_name[i]
+                        )/2
+                    ], 
+                    y=[0.6,0.6,0.4],
+                    tx=aa_x_position_name[i] + (
+                        aa_x_position_name[i+1] - aa_x_position_name[i]
+                    )/4,
+                    ty=1.0,
+                    s=i+1
+                )
+            elif ion_type in 'xyz':
+                return dict(
+                    x=[
+                        aa_x_position_name[i], 
+                        aa_x_position_name[i] - (
+                            aa_x_position_name[i+1] - aa_x_position_name[i]
+                        )/2, 
+                        aa_x_position_name[i] - (
+                            aa_x_position_name[i+1] - aa_x_position_name[i]
+                        )/2
+                    ],
+                    y=[-0.6, -0.6, -0.4],
+                    tx=aa_x_position_name[i] - (
+                        aa_x_position_name[i+1] - aa_x_position_name[i]
+                    )/4,
+                    ty=-1.0,
+                    s=nAA-i
+                )
+        for i, cov in enumerate(covs[:nAA]):
+            if cov:
+                pos = get_position_name(ion_type, i)
+                self.fig.add_trace(
+                    go.Scatter(
+                        x=pos['x'],
+                        y=pos['y'],
+                        mode="lines",
+                        showlegend=False,
+                        marker_color=color,
+                        line_width=1,
+                        hoverinfo='skip'
+                    ),
+                    row=self.row,
+                    col=self.col,
+                )
+                self.fig.add_annotation(
+                    dict(
+                        text=f"{ion_type}{pos['s']}",
+                        x=pos['tx'],
+                        y=pos['ty'],
+                        showarrow=False,
+                        font_size=self.font_size_coverage
+                    ),
+                    row=self.row,
+                    col=self.col,
+                )
