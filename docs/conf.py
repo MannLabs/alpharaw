fcc83c21c19b15ad48b8cff06ea848d586337752
GeorgWa <gewaelbe@googlemail.com>
2023-12-06 00:22:44 +0100

    Bump version: 0.2.0 → 0.3.0

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ project = 'alpharaw'
 copyright = '2022, Mann Labs, MPIB'
 author = 'Mann Labs, MPIB'
 
-release = "0.2.0"
+release = "0.3.0"
 
 # -- General configuration ---------------------------------------------------
 
