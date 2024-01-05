5f86048c01a7d96db45a93af2da92e8ad3a94c62
jalew188 <jalew188@gmail.com>
2024-01-05 22:17:20 +0100

    Bump version: 0.4.0 → 0.4.1

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ project = 'alpharaw'
 copyright = '2022, Mann Labs, MPIB'
 author = 'Mann Labs, MPIB'
 
-release = "0.4.0"
+release = "0.4.1"
 
 # -- General configuration ---------------------------------------------------
 
