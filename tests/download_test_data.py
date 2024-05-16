ab67b7e6d0a0895decd3cc3ff8e0b15bbef9f371
jalew188 <jalew188@gmail.com>
2024-05-16 22:59:17 +0200

    FEAT download_test_data for testing

---
+++ tests/download_test_data.py
@@ -0,0 +1,25 @@
+import requests
+
+def download_file(url, local_filename):
+    # NOTE the stream=True parameter below
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(local_filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192): 
+                # If you have chunk encoded response uncomment if
+                # and set chunk_size parameter to None.
+                #if chunk: 
+                f.write(chunk)
+
+url_template = "https://datashare.biochem.mpg.de/s/GOiZGGOhrHzS54M/download?path=%2F&files={}"
+output_template = "../nbs_tests/test_data/{}"
+test_files = [
+    "20201215_sswath_1min_5ug_1.wiff",
+    "20201215_sswath_1min_5ug_1.wiff.scan",
+    "iRT.raw",
+    "iRT_DIA.raw",
+    "multinotch.raw",
+]
+for test_file in test_files:
+    print(f"Downding {test_file}...")
+    download_file(url_template.format(test_file), output_template.format(test_file))
