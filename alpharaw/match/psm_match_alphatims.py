cab7138f1e242c2b1e25a9800a54bd80fe7fbe0d
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2026-03-17 10:22:09 +0100

    formatting

--- alpharaw/match/psm_match_alphatims.py
+++ alpharaw/match/psm_match_alphatims.py
@@ -1,16 +1,14 @@
 # TODO to be removed
 
-_DEPRECATION_MSG = "has been moved to alphaviz and will be removed from alpharaw in a future version."
+_DEPRECATION_MSG = (
+    "has been moved to alphaviz and will be removed from alpharaw in a future version."
+)
 
 
 def load_ms_data_tims(*args, **kwargs):
-    raise DeprecationWarning(
-        f"load_ms_data_tims {_DEPRECATION_MSG}"
-    )
+    raise DeprecationWarning(f"load_ms_data_tims {_DEPRECATION_MSG}")
 
 
 class PepSpecMatch_AlphaTims:
     def __init__(self, *args, **kwargs):
-        raise DeprecationWarning(
-            f"PepSpecMatch_AlphaTims {_DEPRECATION_MSG}"
-        )
+        raise DeprecationWarning(f"PepSpecMatch_AlphaTims {_DEPRECATION_MSG}")
